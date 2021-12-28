edfb99c2c378439757f1987ffae0ebfadba07ba9
John Fu <fu.johnyh@gmail.com>
2021-12-28 15:24:42 -0800

    Complete calculation without taxes and insurances

--- mortgage.py
+++ mortgage.py
@@ -1,1 +1,10 @@
 # wikipedia reference: https://en.wikipedia.org/wiki/Mortgage_calculator
+from mortgage_functions import monthly_payment
+# Getting the variables from the user (r, N, P)
+r = float(input('What is your annual interest rate in percentage? ')) /100 / 12
+n = float(input("How long is your loan's term in months? "))
+p = float(input("What is your principal amount? "))
+
+# Calculate monthly payment
+c = monthly_payment(p, r, n)
+print('Your monthly payment is $' + str(c))
--- mortgage_functions.py
+++ mortgage_functions.py
@@ -1,6 +1,15 @@
 def monthly_payment(principal, monthly_rate, term_months):
+	"""
+	Returns the monthly payment amount given the principal, monthly rate, and term.
+
+	>>> monthly_payment(200000, 0.065 / 12, 360)
+	1264.14
+
+	"""
 	if monthly_rate != 0:
-		return (monthly_rate * principal * ((1 + monthly_rate)**term_months)) / (((1 + monthly_rate)**term_months) - 1)
+		monthly_cost = (monthly_rate * principal * ((1 + monthly_rate)**term_months)) / (((1 + monthly_rate)**term_months) - 1)
+		return round(monthly_cost, 2)
 	else:
-		return principal / term_months
+		monthly_cost = principal / term_months
+		return round(monthly_cost, 2)
 
