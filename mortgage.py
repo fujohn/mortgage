cc39467079acab15b59daf42d2f535bb2c7e5d60
John Fu <fu.johnyh@gmail.com>
2021-12-28 16:03:26 -0800

    Add amortization schedule calculation

--- mortgage.py
+++ mortgage.py
@@ -1,10 +1,16 @@
 # wikipedia reference: https://en.wikipedia.org/wiki/Mortgage_calculator
-from mortgage_functions import monthly_payment
+from mortgage_functions import monthly_payment, amortization_schedule
+
 # Getting the variables from the user (r, N, P)
-r = float(input('What is your annual interest rate in percentage? ')) /100 / 12
+r = float(input('What is your annual interest rate in percentage? ')) /100 
 n = float(input("How long is your loan's term in months? "))
 p = float(input("What is your principal amount? "))
 
 # Calculate monthly payment
 c = monthly_payment(p, r, n)
 print('Your monthly payment is $' + str(c))
+
+# Amortization
+a = amortization_schedule(p, c, r)
+for key, values in a.items():
+	print(key, values)
--- mortgage_functions.py
+++ mortgage_functions.py
@@ -1,4 +1,4 @@
-def monthly_payment(principal, monthly_rate, term_months):
+def monthly_payment(principal, rate, term_months):
 	"""
 	Returns the monthly payment amount given the principal, monthly rate, and term.
 
@@ -6,6 +6,7 @@ def monthly_payment(principal, monthly_rate, term_months):
 	1264.14
 
 	"""
+	monthly_rate = rate / 12
 	if monthly_rate != 0:
 		monthly_cost = (monthly_rate * principal * ((1 + monthly_rate)**term_months)) / (((1 + monthly_rate)**term_months) - 1)
 		return round(monthly_cost, 2)
@@ -13,3 +14,19 @@ def monthly_payment(principal, monthly_rate, term_months):
 		monthly_cost = principal / term_months
 		return round(monthly_cost, 2)
 
+
+def amortization_schedule(principal, monthly_payment, rate):
+	"""
+	Returns a dictionary where the key represents the month into the term (N) and the value is a list [Interest, principal paid, remaining principal]
+	"""
+	amortization = {}
+	monthly_rate = rate / 12
+	month_into_term = 0
+	while principal > 0:
+		month_into_term += 1
+		interest = principal * monthly_rate
+		principal_paying = min(monthly_payment - interest, principal)
+		principal -= principal_paying
+		amortization[month_into_term] = [round(interest, 2), round(principal_paying, 2), round(principal, 2)]
+	return amortization
+		
